23ec2a83c27729288c16dc25892e425649e44530
Matheus Finatto <matheusfinatto2@gmail.com>
2022-10-27 16:27:00 -0300

    url change

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/movies-library/' element={<Home />} />
           <Route path='/movie/:id' element={<Movie />} />
           <Route path='search' element={<Search />} />
         </Routes>
